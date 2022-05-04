714957fb781f5f6e07fef52127e1195dd7b83b6b
AjlaK <kundicajla@gmail.com>
2022-05-04 23:04:29 +0200

    Ciao da Ajla

--- TestBrendiGit/Program.cs
+++ TestBrendiGit/Program.cs
@@ -7,6 +7,7 @@ namespace TestBrendiGit
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Brendi!");
+            Console.WriteLine("Hello da Ajla!");
         }
     }
 }
